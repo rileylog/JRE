2639256498d3d6693f1d6195136c46e061a2a306
Logan Riley <rileylog@gmail.com>
2013-10-15 22:26:46 -0600

    Setting up initial ruby-version file, powenv file and ruby-gemset file. Also adding the gem files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JRE::Application.config.secret_token = '5ba23b9768480c189e59e240e338622beaf99672a285cf1eb8a63e2da355641dfc8bac0cf61b2b604cf278e258ea38a34470472ca1cb37263a08b76d624b9c36'
